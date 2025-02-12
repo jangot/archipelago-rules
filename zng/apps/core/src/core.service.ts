7fd9d8afba001a45792cb513568904eaeaa44548
Mike LeDuc <ml@zirtue.com>
2025-02-12 09:31:52 -0600

    Updated packages and fixed build error

--- zng/apps/core/src/core.service.ts
+++ zng/apps/core/src/core.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { IDataService } from './data/idata.service';
 import { v4 } from 'uuid';
-import { runOnTransactionCommit, runOnTransactionComplete, runOnTransactionRollback, Transactional } from 'typeorm-transactional';
+import { Transactional } from 'typeorm-transactional';
 import { withTransactionHandler } from '@library/shared/common/data/withtransaction.handler';
 
 @Injectable()
@@ -9,15 +9,6 @@ export class CoreService {
   constructor(
     private readonly dataService: IDataService,
     private readonly logger: Logger) { }
-
-  // Obviously this should be in a UserService class, not this class.
-  public async getUserById(id: string) {
-    return await this.dataService.users.getById(id);
-  }
-
-  public async getUserByEmail(email: string) {
-    return await this.dataService.users.getByEmail(email);
-  }
 
   // For TypeORM Transactional testing purposes
   // On first call it will create a lender, borrower and a loan. Plus logs on transaction commit and complete
