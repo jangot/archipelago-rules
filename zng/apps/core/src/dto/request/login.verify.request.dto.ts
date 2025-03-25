02de3769ddb9ab39ec345469cfcf879db0f1ef1e
Alexander Krasilnikov <shpdogville@gmail.com>
2025-03-25 19:07:06 +0500

    fixing login verification (class-validator + class-transformer issue)

--- zng/apps/core/src/dto/request/login.verify.request.dto.ts
+++ zng/apps/core/src/dto/request/login.verify.request.dto.ts
@@ -6,7 +6,11 @@ import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 @ApiSchema({ name: 'loginVerifyRequest' })
 export class LoginVerifyRequestDto extends LoginRequestDto {
   @ApiProperty({ description: 'Id of the User', type: String, required: true })
-  @Expose({ name: 'id' }) // Example of mapping the Entity 'id' field to the userId field here
+  // TODO: Calling @Expose() from 'class-transformer' to map the 'id' field to the 'userId' field will cause an issue
+  // AS this happens right before 'class-validatior' will run its checks - these checks will fail but seems that field is still in the payload
+  // see simillar issue in official repo: https://github.com/typestack/class-validator/issues/1559
+  // Commented out for now as there no further usage of 'class-transformer' methods for login verification flow
+  // @Expose({ name: 'id' }) // Example of mapping the Entity 'id' field to the userId field here
   @IsString()
   @IsUUID()
   userId: string; // User id - uuid
