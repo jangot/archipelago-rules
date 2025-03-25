3e2a44a746eea6903a1f6f8c0122e1b4b483838c
Alexander Krasilnikov <shpdogville@gmail.com>
2025-03-25 19:34:26 +0500

    bring Expose decorator back but now with correct params

--- zng/apps/core/src/dto/request/login.verify.request.dto.ts
+++ zng/apps/core/src/dto/request/login.verify.request.dto.ts
@@ -6,11 +6,7 @@ import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 @ApiSchema({ name: 'loginVerifyRequest' })
 export class LoginVerifyRequestDto extends LoginRequestDto {
   @ApiProperty({ description: 'Id of the User', type: String, required: true })
-  // TODO: Calling @Expose() from 'class-transformer' to map the 'id' field from the 'userId' field will cause an issue
-  // AS this happens right before 'class-validatior' will run its checks - these checks will fail but seems that field is still in the payload
-  // see simillar issue in official repo: https://github.com/typestack/class-validator/issues/1559
-  // Commented out for now as there no further usage of 'class-transformer' methods for login verification flow
-  // @Expose({ name: 'id' }) // Example of mapping the Entity 'id' field to the userId field here
+  @Expose()
   @IsString()
   @IsUUID()
   userId: string; // User id - uuid
