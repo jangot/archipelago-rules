fa44753253e5f36a2632e8eb074da98fafd13894
Alexander Krasilnikov <shpdogville@gmail.com>
2025-03-27 18:54:44 +0500

    local debug: webpack sourceMaps configuration udpate to activate breakpoints

--- zng/webpack.config.js
+++ zng/webpack.config.js
@@ -1,4 +1,8 @@
 module.exports = (options) => ({
   ...options,
-  devtool: 'source-map',
+  devtool: 'inline-source-map', // Use inline source maps for better path resolution
+  output: {
+    ...options.output,
+    sourceMapFilename: '[file].map', // Ensure source maps are generated in the same directory as the output file
+  },
 });
