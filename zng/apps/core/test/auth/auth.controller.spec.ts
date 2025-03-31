2cc0b684f2430073451284e3b60ac9ceb06f52c3
Alexander Krasilnikov <shpdogville@gmail.com>
2025-03-31 18:24:40 +0500

    WIP on Auth e2e: complete module emulation and transparent flow for tests

--- zng/apps/core/test/auth/auth.controller.spec.ts
+++ zng/apps/core/test/auth/auth.controller.spec.ts
@@ -1,46 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from '../../src/auth/auth.controller';
 import { AuthService } from '../../src/auth/auth.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { RegistrationService } from '../../src/auth/registration.service';
 import * as request from 'supertest';
-import { INestApplication } from '@nestjs/common';
-import { CqrsModule } from '@nestjs/cqrs';
+import { INestApplication, Logger } from '@nestjs/common';
+import { CommandBus, CqrsModule } from '@nestjs/cqrs';
 import { IBackup } from 'pg-mem';
 import { memoryDataSourceForTests } from '../postgress-memory-datasource';
 import { addTransactionalDataSource, initializeTransactionalContext, StorageDriver } from 'typeorm-transactional';
 import { DataSource } from 'typeorm';
-import { AuthModule } from '../../src/auth/auth.module';
+import { LoginCommandHandlers } from '../../src/auth/login/commands';
+import { UsersModule } from '../../src/users';
+import { DataModule } from '../../src/data';
+import { DomainModule } from '../../src/domain/domain.module';
+import { JwtModule } from '@nestjs/jwt';
+import { CustomAuthStrategies } from '../../src/auth/strategies';
+import { CustomAuthGuards } from '../../src/auth/guards';
+import { RegistrationCommandHandlers } from '../../src/auth/registration/commands';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter, DomainExceptionsFilter } from '@library/shared/common/filters';
+import { LoginInitiateCommandHandler } from '../../src/auth/login/commands/login.initiate.command';
+import { DomainExceptionCode } from '@library/shared/common/exceptions/domain';
 
 // Jest can not 'understand' camelcase-keys ESM properly. Mock it to avoid errors.
 jest.mock('camelcase-keys', () => ({
   camelcaseKeys: jest.fn(),
 }));
+jest.setTimeout(300000);
 
 describe('AuthController - Negative Test Cases', () => {
   let app: INestApplication;
   let databaseBackup: IBackup;
+
+  let authService: AuthService;
+  let authServiceLoginInitiateSpy: jest.SpyInstance;
+
+  let commandBus: CommandBus;
+  let commandBusSpy: jest.SpyInstance;
+  let loginInitiateHandler: LoginInitiateCommandHandler;
+  let loginInitiateHandlerSpy: jest.SpyInstance;
 
   beforeAll(async () => {
     const memoryDBinstance = await memoryDataSourceForTests();
     const { dataSource, database } = memoryDBinstance;
     initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
     
+    // Build the complete copy of AuthModule
     const module: TestingModule = await Test.createTestingModule({
-      imports: [CqrsModule, ConfigModule.forRoot(), AuthModule],
+      imports: [CqrsModule, UsersModule, ConfigModule.forRoot(), DataModule, DomainModule, JwtModule],
       controllers: [AuthController],
       providers: [
+        Logger,
         AuthService,
-        ConfigService,
         RegistrationService,
+        ...CustomAuthStrategies,
+        ...CustomAuthGuards,
+        ...RegistrationCommandHandlers,
+        ...LoginCommandHandlers,
+        // Use another way of ExceptionFilters injection as it is only way to have them in test module
+        { provide: APP_FILTER, useClass: AllExceptionsFilter }, { provide: APP_FILTER, useClass: DomainExceptionsFilter },
       ],
     }).overrideProvider(DataSource)
       .useValue(addTransactionalDataSource(dataSource))
       .compile();
 
+
+    authService = module.get<AuthService>(AuthService);
+    commandBus = module.get<CommandBus>(CommandBus);
+    loginInitiateHandler = module.get<LoginInitiateCommandHandler>(LoginInitiateCommandHandler);
+
     databaseBackup = database.backup();
     app = module.createNestApplication();
     await app.init();
+
+
   });
 
   afterAll(async () => {
@@ -48,16 +82,86 @@ describe('AuthController - Negative Test Cases', () => {
   });
 
   beforeEach(() => {
+    jest.restoreAllMocks();
     databaseBackup.restore();
   });
 
   describe('login', () => {
-    it('should throw BadRequestException if email and phoneNumber are missing', async () => {
+    it('email and phoneNumber are missing - should throw an expection 400 from controller', async () => {
+      authServiceLoginInitiateSpy = jest.spyOn(authService, 'initiateLoginSession');
       await request(app.getHttpServer())
         .post('/auth/login')
         .send({ email: '', phoneNumber: '' })
         .expect(400);
+
+      // Check that we even to not call AuthService
+      expect(authServiceLoginInitiateSpy).not.toHaveBeenCalled();
     });
+
+    it('email and phoneNumber both provided - should throw an expection 400 from controller', async () => {
+      authServiceLoginInitiateSpy = jest.spyOn(authService, 'initiateLoginSession');
+      await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: 'abc', phoneNumber: 'abc' })
+        .expect(400);
+
+      // Check that we even to not call AuthService
+      expect(authServiceLoginInitiateSpy).not.toHaveBeenCalled();
+    });
+
+    it('invalid email provided - should throw an expection 404 from controller', async () => {
+      authServiceLoginInitiateSpy = jest.spyOn(authService, 'initiateLoginSession');
+      loginInitiateHandlerSpy = jest.spyOn(loginInitiateHandler, 'execute');
+      commandBusSpy = jest.spyOn(commandBus, 'execute');
+      const response = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: 'abc', phoneNumber: '' })
+        .expect(404);
+
+      expect(response.body).toEqual(expect.objectContaining({
+        statusCode: 404,
+        errorCode: DomainExceptionCode.EntityNotFound,
+        message: 'No matching user found',
+      }));
+
+      // Check that once call AuthService
+      expect(authServiceLoginInitiateSpy).toHaveBeenCalledTimes(1);
+      expect(authServiceLoginInitiateSpy).toHaveBeenCalledWith({ 'email': 'abc', 'phoneNumber': '' });
+      // Check that CommandBus was called once
+      expect(commandBusSpy).toHaveBeenCalledTimes(1);
+      expect(commandBusSpy).toHaveBeenCalledWith({ 'payload': { 'contact': 'abc', 'contactType': 'email' } });
+      // Check that proper command handler was called with expected payload
+      expect(loginInitiateHandlerSpy).toHaveBeenCalledTimes(1);
+      expect(loginInitiateHandlerSpy).toHaveBeenCalledWith({ 'payload': { 'contact': 'abc', 'contactType': 'email' } });
+    });
+
+    it('invalid phone provided - should throw an expection 404 from controller', async () => {
+      authServiceLoginInitiateSpy = jest.spyOn(authService, 'initiateLoginSession');
+      loginInitiateHandlerSpy = jest.spyOn(loginInitiateHandler, 'execute');
+      commandBusSpy = jest.spyOn(commandBus, 'execute');
+      const response = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: '', phoneNumber: 'abc' })
+        .expect(404);
+
+      expect(response.body).toEqual(expect.objectContaining({
+        statusCode: 404,
+        errorCode: DomainExceptionCode.EntityNotFound,
+        message: 'No matching user found',
+      }));
+
+      // Check that once call AuthService
+      expect(authServiceLoginInitiateSpy).toHaveBeenCalledTimes(1);
+      expect(authServiceLoginInitiateSpy).toHaveBeenCalledWith({ 'email': '', 'phoneNumber': 'abc' });
+      // Check that CommandBus was called once
+      expect(commandBusSpy).toHaveBeenCalledTimes(1);
+      expect(commandBusSpy).toHaveBeenCalledWith({ 'payload': { 'contact': 'abc', 'contactType': 'phoneNumber' } });
+      // Check that proper command handler was called with expected payload
+      expect(loginInitiateHandlerSpy).toHaveBeenCalledTimes(1);
+      expect(loginInitiateHandlerSpy).toHaveBeenCalledWith({ 'payload': { 'contact': 'abc', 'contactType': 'phoneNumber' } });
+    });
+
+    
   });
 
   describe('verifyLogin', () => {
