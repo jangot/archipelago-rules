a4a6624682c985bde1ccf5ec5b344c202cf1e680
Mike LeDuc <ml@zirtue.com>
2025-06-18 15:21:38 -0500

    Updated packages and working on SQS publisher

--- zng/libs/shared/src/common/messages/sqs-message-publisher.ts
+++ zng/libs/shared/src/common/messages/sqs-message-publisher.ts
@@ -1,14 +1,35 @@
 import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
 import { MessagePayloadSqs } from './interfaces/message-payload';
-import { Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { BaseMessagePublisher } from './base.message-publisher';
+import { ConfigService } from '@nestjs/config';
 
+@Injectable()
 export class SqsMessagePublisher extends BaseMessagePublisher<SendMessageCommand> {
   private readonly logger = new Logger(SqsMessagePublisher.name);
-  constructor(private readonly sqsClient: SQSClient) {
+  private sqsClient: SQSClient;
+
+  constructor(protected readonly configService: ConfigService) {
     super();
   }
-  
+
+  // Lazily create the SQS client
+  private getSqsClient(): SQSClient {
+    if (!this.sqsClient) {
+      this.sqsClient = new SQSClient({
+        region: this.configService.getOrThrow<string>('AWS_REGION'),
+        profile: this.getSqsCredentials(),
+      });
+    }
+
+    return this.sqsClient;
+  }
+
+  // Credentials are only needed for localstack (local development)
+  private getSqsCredentials(): string | undefined {
+    return this.configService.get('IS_LOCAL') === '1' ? this.configService.get<string>('AWS_PROFILE') || 'localstack' : undefined;
+  }
+
   protected async createMessagePayload(payload: MessagePayloadSqs, message: string): Promise<SendMessageCommand> {
     return new SendMessageCommand({
       QueueUrl: payload.queueUrl,
@@ -22,7 +43,8 @@ export class SqsMessagePublisher extends BaseMessagePublisher<SendMessageCommand
   }
 
   protected async sendMessage(message: SendMessageCommand): Promise<string> {
-    const result = await this.sqsClient.send(message);
+    const sqsClient = this.getSqsClient();
+    const result = await sqsClient.send(message);
 
     if (!result.MessageId) {
       this.logger.error('MessageId is required');
