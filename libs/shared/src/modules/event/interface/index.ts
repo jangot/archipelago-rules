a216629997e947c1e5cc3f670f28b237115dc60a
Mike LeDuc <ml@zirtue.com>
2025-07-25 17:51:06 -0500

    renamed to all lowercase to fix build issue

--- libs/shared/src/modules/event/interface/index.ts
+++ libs/shared/src/modules/event/interface/index.ts
@@ -1,7 +1,7 @@
 import { IEvent } from '@nestjs/cqrs';
 
-export * from './ievent-module-options';
 export * from './ievent-module-config';
+export * from './ievent-module-options';
 export * from './isns-notification';
 export * from './isqs-concumer-instance';
 
