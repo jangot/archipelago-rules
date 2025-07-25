26628447de5a927b45215276cccac1d86828546d
Pavel Pulin <pavel.pulin@vallettasoftware.com>
2025-07-25 15:07:50 +0300

    remove trash

--- libs/shared/src/common/message/aws/sqs-client.ts
+++ libs/shared/src/common/message/aws/sqs-client.ts
@@ -4,7 +4,6 @@ import { ConfigService } from '@nestjs/config';
 export function getSqsClient(configService: ConfigService): SQSClient {
   return new SQSClient({
     region: configService.getOrThrow<string>('AWS_REGION'),
-    endpoint: configService.getOrThrow<string>('AWS_ENDPOINT_URL'),
     // Credentials are only needed for localstack (local development)
     profile: configService.get('IS_LOCAL') === '1' ? configService.get<string>('AWS_PROFILE') || 'localstack' : undefined,
   });
