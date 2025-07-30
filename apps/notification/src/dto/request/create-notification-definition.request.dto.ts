6b224614da67d2cf8782f09a524cfb2afa9b0cde
Pavel Pulin <pavel.pulin@vallettasoftware.com>
2025-07-30 12:21:51 +0300

    add dataItems validation

--- apps/notification/src/dto/request/create-notification-definition.request.dto.ts
+++ apps/notification/src/dto/request/create-notification-definition.request.dto.ts
@@ -1,5 +1,7 @@
-import { IsArray, IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { NotificationDataItems } from '@library/entity/enum/notification-data-items';
+import { IsArray, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { ContainsRequiredValues } from '@library/shared';
+import { ApiProperty } from '@nestjs/swagger';
 
 /**
  * DTO for creating a notification definition
@@ -8,14 +10,28 @@ export class CreateNotificationDefinitionRequestDto {
   /**
    * Name of the notification definition
    */
+  @ApiProperty({
+    description: 'Name of the notification definition',
+    example: 'payment_reminder',
+    required: false,
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
   /**
    * Array of notification types that this definition supports
+   * Must always contain 'user' when provided
    */
+  @ApiProperty({
+    description: 'Array of notification types that this definition supports. Must always contain user.',
+    example: [NotificationDataItems.User, NotificationDataItems.Loan],
+    required: false,
+    isArray: true,
+    enum: NotificationDataItems,
+  })
   @IsArray()
   @IsEnum(NotificationDataItems, { each: true })
+  @ContainsRequiredValues([NotificationDataItems.User], { message: 'dataItems must always contain user' })
   dataItems: NotificationDataItems[];
 }
--- apps/notification/src/dto/request/update-notification-definition.request.dto.ts
+++ apps/notification/src/dto/request/update-notification-definition.request.dto.ts
@@ -1,6 +1,7 @@
-import { IsArray, IsEnum, IsOptional, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 import { NotificationDataItems } from '@library/entity/enum/notification-data-items';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsEnum, IsOptional, IsString } from 'class-validator';
+import { ContainsRequiredValues } from '@library/shared';
 
 /**
  * DTO for updating an existing notification definition
@@ -8,11 +9,11 @@ import { NotificationDataItems } from '@library/entity/enum/notification-data-it
 export class UpdateNotificationDefinitionRequestDto {
   /**
    * Updated name for the notification definition
-   * @example 'Updated Payment Reminder'
+   * @example 'payment_reminder'
    */
   @ApiProperty({
     description: 'Updated name for the notification definition',
-    example: 'Updated Payment Reminder',
+    example: 'payment_reminder',
     required: false,
   })
   @IsOptional()
@@ -21,8 +22,18 @@ export class UpdateNotificationDefinitionRequestDto {
 
   /**
    * Array of notification types that this definition supports
+   * Must always contain 'user' when provided
    */
+  @ApiProperty({
+    description: 'Array of notification types that this definition supports. Must always contain user.',
+    example: [NotificationDataItems.User, NotificationDataItems.Loan],
+    required: false,
+    isArray: true,
+    enum: NotificationDataItems,
+  })
   @IsArray()
+  @IsOptional()
   @IsEnum(NotificationDataItems, { each: true })
-  dataItems: NotificationDataItems[];
+  @ContainsRequiredValues([NotificationDataItems.User], { message: 'dataItems must always contain user' })
+  dataItems?: NotificationDataItems[];
 }
