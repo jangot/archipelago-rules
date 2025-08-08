f994123af959621420037e4ed79fd14e27ffb3a3
Pavel Pulin <pavel.pulin@vallettasoftware.com>
2025-08-08 15:41:16 +0300

    run migrations

---
+++ db/config/datasource.ts
@@ -0,0 +1,19 @@
+import { config as loadEnvironments } from 'dotenv';
+import { DataSource } from 'typeorm';
+import { ZngNamingStrategy } from '@library/extensions/typeorm/zng-naming.strategy';
+import { AllEntities } from '@library/shared/domain/entity';
+
+loadEnvironments();
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '0'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: AllEntities,
+  migrations: ['db/migrations/*.up.sql'],
+  namingStrategy: new ZngNamingStrategy(),
+  logging: process.env.TYPE_ORM_LOGGING === 'true' ? ['query', 'error'] : false,
+});
